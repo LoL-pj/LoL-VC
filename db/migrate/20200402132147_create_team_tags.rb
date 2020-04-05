cf091cf8f3a51a128d76529f4e5baed844cb42d5
yuko-okayasu <yuko_okayasu@wondershake.com>
2020-04-05 21:53:09 +0900

    :+1: change migration file order

--- db/migrate/20200402132147_create_team_tags.rb
+++ db/migrate/20200402132147_create_team_tags.rb
@@ -1,8 +1,8 @@
 class CreateTeamTags < ActiveRecord::Migration[6.0]
   def change
     create_table :team_tags do |t|
-      t.references :team_id, foreign_key: true, null: false
-      t.references :tag_id, foreign_key: true, null: false
+      t.references :team, foreign_key: true, null: false
+      t.references :tag, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_141126) do
+ActiveRecord::Schema.define(version: 2020_04_02_132147) do
 
-  create_table "champions", force: :cascade do |t|
+  create_table "champions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "ranks", force: :cascade do |t|
+  create_table "ranks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "tags", force: :cascade do |t|
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namet", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "team_champions", force: :cascade do |t|
+  create_table "team_champions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "team_id"
     t.integer "champion_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "team_tags", force: :cascade do |t|
-    t.integer "team_id_id", null: false
-    t.integer "tag_id_id", null: false
+  create_table "team_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "team_id", null: false
+    t.bigint "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["tag_id_id"], name: "index_team_tags_on_tag_id_id"
-    t.index ["team_id_id"], name: "index_team_tags_on_team_id_id"
+    t.index ["tag_id"], name: "index_team_tags_on_tag_id"
+    t.index ["team_id"], name: "index_team_tags_on_team_id"
   end
 
-  create_table "teams", force: :cascade do |t|
+  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.string "summoner_name"
@@ -54,16 +54,16 @@ ActiveRecord::Schema.define(version: 2020_04_02_141126) do
     t.string "discord"
     t.string "game_type"
     t.string "password", null: false
-    t.integer "rank_id"
-    t.integer "champion_id"
+    t.bigint "rank_id"
+    t.bigint "champion_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["champion_id"], name: "index_teams_on_champion_id"
     t.index ["rank_id"], name: "index_teams_on_rank_id"
   end
 
-  add_foreign_key "team_tags", "tag_ids"
-  add_foreign_key "team_tags", "team_ids"
+  add_foreign_key "team_tags", "tags"
+  add_foreign_key "team_tags", "teams"
   add_foreign_key "teams", "champions"
   add_foreign_key "teams", "ranks"
 end
