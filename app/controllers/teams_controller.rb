d1a6472c6daaa9c888a4adb18ab05f315069eb91
rutko <t.222cm@gmail.com>
2020-04-12 00:20:59 +0900

    ✨Create form view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,28 @@
 class TeamsController < ApplicationController
   def index
+    @teams = Team.all
+  end
+
+  def show
+    @team = Team.find_by(id: params[:id])
   end
 
   def new
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(teams_params)
+
+    respond_to do |format|
+      if @team.save
+        format.html { redirect_to @tema, notice: '募集しました' }
+        format.json { render :show, status: :created, location: @tema }
+      else
+        format.html { render :new }
+        format.json { render json: @team.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -15,5 +32,9 @@ class TeamsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def team_params
+    params.require(:team).permit(resources :teams)
   end
 end
