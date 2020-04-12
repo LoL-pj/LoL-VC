3ce5cc751b4a33baf38abb4cfc775b0b764ada75
rutko <t.222cm@gmail.com>
2020-04-13 01:59:48 +0900

    ♻️Add Routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'teams#index'
   # Teams
-  get  'teams/show'   => 'teams#show'
-  get  'teams/new'    => 'teams#new'
-  post 'teams/create' => 'teams#create'
+  resources :teams, only: [:show, :new, :create, :edit, :update, :destroy]
 end
