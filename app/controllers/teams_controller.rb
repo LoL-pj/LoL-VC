26f0240e7fa4ba0fb1ca2c824dad1ab164251706
rutko <t.222cm@gmail.com>
2020-04-13 01:59:15 +0900

    ✨Create Edit Destroy action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,11 @@
 class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
+
   def index
     @teams = Team.all
   end
 
   def show
-    @team = Team.find_by(id: params[:id])
   end
 
   def new
@@ -29,12 +30,29 @@ class TeamsController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @team.update(team_params)
+        format.html { redirect_to @team, notice: '更新しました' }
+        format.json { render :show, status: :ok, location: @team }
+      else
+        format.html { render :edit }
+        format.json { render json: @team.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @team.destroy
+    respond_to do |format|
+      format.html { redirect_to root_url, notice: '削除しました'}
+    end
   end
 
   def team_params
     params.require(:team).permit(:title, :summoner_name, :game_type, :discord, :skype, :body, :password)
   end
+end
+
+def set_team
+  @team = Team.find(params[:id])
 end
