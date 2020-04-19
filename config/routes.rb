84e81062d306f6be8426db8c2c86a316821c2d20
rutko <t.222cm@gmail.com>
2020-04-19 15:51:39 +0900

    ♻️Refactor routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'teams#index'
   # Teams
-  resources :teams, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :teams, except: [:index]
 end
