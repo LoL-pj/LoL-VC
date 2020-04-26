e6c8573645c071a99b90807631f8335abfbfe806
yuko-okayasu <yuko_okayasu@wondershake.com>
2020-04-27 01:19:39 +0900

    :sparkles: create search action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,19 +1,13 @@
 class TeamsController < ApplicationController
-  def index
+
+  def search
+    @search_params = team_search_params
+    @teams = Team.search(@search_params)
   end
 
-  def new
-  end
+  private
 
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+  def team_search_params
+    params.fetch(:search, {}).permit(:keyword, game_type_ids: [], rank_ids: [], champion_ids: [])
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,20 @@
 class Team < ApplicationRecord
+  has_many :team_game_types
+  has_many :team_ranks
+  has_many :team_champions
   has_many :game_types, through: :team_game_types
   has_many :ranks, through: :team_ranks
   has_many :champions, through: :team_champions
   has_many :tags, through: :team_tags
+
+  scope :search, -> (search_params) do
+    return if search_params.blank?
+
+    keyword_like(search_params[:keyword]).game_type_like(search_params[:game_type_ids]).rank_like(search_params[:rank_ids]).champion_like(search_params[:champion_ids])
+  end
+
+  scope :keyword_like, -> (keyword) { where('title LIKE ? OR body LIKE ?', "%#{keyword}%", "%#{keyword}%") if keyword.present? }
+  scope :game_type_like, -> (game_type) { joins(:game_types).where(id: game_type) if game_type.present? }
+  scope :rank_like, -> (rank) { joins(:ranks).where(id: rank) if rank.blank? }
+  scope :champion_like, -> (champion) { joins(:champions).where(id: champion) if champion.blank? }
 end
