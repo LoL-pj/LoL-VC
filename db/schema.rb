4e0bd45ea9628838480b0b86b9f7ec7ab2a433ef
rutko <t.222cm@gmail.com>
2020-05-09 23:39:33 +0900

    ♻️Fix summoner name column not null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_140852) do
+ActiveRecord::Schema.define(version: 2020_05_09_142622) do
 
   create_table "champions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -75,12 +75,12 @@ ActiveRecord::Schema.define(version: 2020_04_25_140852) do
   create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
-    t.string "summoner_name"
+    t.string "summoner_name", null: false
     t.string "skype"
     t.string "discord"
-    t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "team_champions", "champions"
