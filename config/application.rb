a9fa79ce87322e2eaca52be0ac3d60bd34c8ac8f
yuko-okayasu <yuko_okayasu@wondershake.com>
2020-05-11 23:44:04 +0900

    :sparkles: add gem rails-i18n

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module LolVc
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
