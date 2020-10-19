d9d926929dd440c2af8f39a1c12559ac85b13120
rutko <t.222cm@gmail.com>
2020-10-19 23:08:28 +0900

    :🐛Fix timezone from UTC to JST

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module LolVc
     # the framework and any gems in your application.
 
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
