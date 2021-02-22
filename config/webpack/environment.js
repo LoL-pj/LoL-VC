086b02c1c858623a35a2ef8b6f4f79818f874d83
rutko <t.222cm@gmail.com>
2021-02-22 15:19:07 +0900

    ✨Install typescript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
 const { environment } = require('@rails/webpacker')
+const typescript =  require('./loaders/typescript')
 
+environment.loaders.prepend('typescript', typescript)
 module.exports = environment
+
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const path = require("path");
+
+environment.plugins.append(
+    "ForkTsCheckerWebpackPlugin",
+    new ForkTsCheckerWebpackPlugin({
+          typescript: {
+                  configFile: path.resolve(__dirname, "../../tsconfig.json"),
+                },
+          async: false,
+        })
+);
